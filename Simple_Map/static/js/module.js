36c14147b6af4a12f47b12204fd3c119a52c4e88
Sonmi Kim <annkimalways@gmail.com>
2022-05-25 21:28:57 -0500

    new folder

---
+++ Simple_Map/static/js/module.js
@@ -0,0 +1,41 @@
+console.log("working");
+var map = L.map('map').setView([51.505, -0.09], 13);
+//let map = L.map('mapid').setView([40.7, -94.5], 4);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={API_KEY}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(map);
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+//Adding a polygon is as easy:
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+    .setLatLng([51.513, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
+
+    function onMapClick(e) {
+        alert("You clicked the map at " + e.latlng);
+    }
+    
+    map.on('click', onMapClick);
