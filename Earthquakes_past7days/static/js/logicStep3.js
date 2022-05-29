c73633cc3fcd71038d0e91f17a88721108ed8ca3
Sonmi Kim <annkimalways@gmail.com>
2022-05-29 11:59:35 -0500

    Earth quake past 7 days display

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -28,7 +28,7 @@ let map = L.map('mapid', {
   layers: [streets]
 });
 // Pass our map layers into our layers control and add the layers control to the map.
-//L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
@@ -36,20 +36,41 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
 function getRadius(magnitude) {
-    console.log(magnitude);
+    //console.log(magnitude);
   if (magnitude === 0) {
     return 1;
   }
   return magnitude * 4;
 }
+
+function getColor(magnitude) {
+    console.log(magnitude);
+    if (magnitude > 5) {
+        return "#ea2c2c";
+      }
+    if (magnitude > 4) {
+        return "#ea822c";
+    }
+    if (magnitude > 3) {
+        return "#ee9c00";
+    }
+    if (magnitude > 2) {
+        return "#eecc00";
+    }
+    if (magnitude > 1) {
+        return "#d4ee00";
+    }
+    return "#98ee00";
+}
+
 // This function returns the style data for each of the earthquakes we plot on
-// the map. We pass the magnitude of the earthquake into a function
-// to calculate the radius.
+// the map. We pass the magnitude of the earthquake into two separate functions
+// to calculate the color and radius.
 function styleInfo(feature) {
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: "#ffae42",
+      fillColor: getColor(feature.properties.mag),
       color: "#000000",
       radius: getRadius(feature.properties.mag),
       stroke: true,
@@ -60,15 +81,21 @@ function styleInfo(feature) {
 
 
   // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-    
-    // We turn each feature into a circleMarker on the map.    
-    pointToLayer: function(feature, latlng) {
+    L.geoJSON(data, {    
+        // We turn each feature into a circleMarker on the map.    
+        pointToLayer: function(feature, latlng) {
                 //console.log(data);
                 return L.circleMarker(latlng);
             },
-            style: styleInfo
-        }).addTo(map);
+        style: styleInfo,
+
+        // We create a popup for each circleMarker to display the magnitude and
+        //  location of the earthquake after the marker has been created and styled.
+        onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      }
+
+    }).addTo(map);
 });
 
 
