812b702fea1c9b986fdd977ed2f702bc4dad0649
Sonmi Kim <annkimalways@gmail.com>
2022-05-29 22:12:13 -0500

    legend added

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -109,3 +109,27 @@ function styleInfo(feature) {
     }).addTo(earthquakes);
     earthquakes.addTo(map);
 });
+
+var legend = L.control({position: 'bottomright'});
+// Then add all the details for the legend.
+const magnitudes = [0, 1, 2, 3, 4, 5];
+const colors = [
+  "#98ee00",
+  "#d4ee00",
+  "#eecc00",
+  "#ee9c00",
+  "#ea822c",
+  "#ea2c2c"
+];
+legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    for (var i = 0; i < magnitudes.length; i++) {
+        console.log(colors[i]);
+        div.innerHTML +=
+          "<i style='background: " + colors[i] + "'></i> " +
+          magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+     }
+      return div;
+  };
+
+  legend.addTo(map);
